f1ec61b591c8beecaf6cd2b8863d8ff0802d0ffc
Justin Lane <jclane@users.noreply.github.com>
2018-10-08 10:55:38 -0400

    Added docstrings

--- stolen_from_TIL.py
+++ stolen_from_TIL.py
@@ -6,7 +6,36 @@ from time import sleep
 
 
 class Fact:
-    def __init__(self, data):
+    
+    """
+    This is a class to store the JSON data
+    retrieved from get_fact().
+    
+    Class attribue 'post' contains the JSON
+    data for the most recent post.
+    
+    Class attribute 'title' is title of the 
+    post.
+    
+    Class attribute 'url' is the link to the 
+    source of the fact.
+    """
+    
+    
+    def __init__(self, data):  
+        """
+        Initialize Fact object with 'post', 'title', 
+        and 'url' attributes.  
+        
+        Effort is made to clean up the punctuation 
+        and wording of the title by removing 'TIL', 
+        'about', 'that', 'of', and a comma from the 
+        beginning of the string.  A period is added
+        at the end if one is not present and the 
+        first letter of the string is capitalized if
+        not already.
+        """
+        
         self.post = data['data']['children'][0]['data']
         self.id = self.post['id']
         self.title = self.post['title'].replace("TIL", "").strip()
@@ -30,12 +59,25 @@ class Fact:
         self.title = self.title[0].upper() + self.title[1:]
         
     def show(self):
+        """
+        Prints the title and url to the console
+        """
+        
         print("\nFACT:")
         print("\n" + self.title)
         print("\n" + self.url + "\n")
 
 
 def save_fact(data):
+    """
+    Writes the data to the 'facts.csv' file.
+    Will create the file if it does not 
+    already exist.
+    
+    :param data: A list containing the title and url
+        of a Fact object.
+    """
+    
     file_path = "facts.csv"
 
     if os.path.exists(file_path):
@@ -49,6 +91,18 @@ def save_fact(data):
 
 
 def check_for_duplicates(title):
+    """
+    Reads the 'facts.csv' file and compares
+    each line to the 'title' parameter.  If
+    'title' is found returns 'True' else
+    returns 'False'.
+    
+    :param title: Title as string of the Fact object to 
+        be checked.
+    :return: Boolean indicating if 'title' is already
+        present in the 'facts.csv' file.
+    """
+    
     file_path = "facts.csv"
 
     if os.path.exists(file_path):
@@ -64,6 +118,11 @@ def check_for_duplicates(title):
         
 
 def get_fact():
+    """
+    Uses 'requests' module to retrieve most recent post from 
+    /r/todayilearned in JSON format.
+    """
+    
     url = "https://www.reddit.com/r/todayilearned/new/.json"
     resp = requests.get(url, headers={'User-agent': 'Simple Test'})
     data = resp.json()
@@ -75,7 +134,16 @@ def get_fact():
 
 
 def tweet_fact(fact):
-
+    """
+    Uses 'twitter' module to send tweet.
+    
+    Tokens and keys are stored in a seperate file
+    not included in this repo.
+    
+    :param fact: 'title' attribute as string of the Fact object to 
+        be tweeted.
+    """
+    
     with open("keys_file.txt", "r") as keys_file:
         keys = keys_file.readlines()
         TOKEN = keys[0].strip("\n")
